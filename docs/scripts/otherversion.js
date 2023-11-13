b8d5ac4e55383c1c32f2f56c7b5b1a29d767d697
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-11-13 16:33:11 +0530

    updated download page

--- docs/scripts/otherversion.js
+++ docs/scripts/otherversion.js
@@ -1,22 +1,44 @@
-document.addEventListener("DOMContentLoaded", function() {
+/* <script src="https://cdnjs.cloudflare.com/ajax/libs/marked/2.0.3/marked.min.js"></script> */
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Ensure marked.js is loaded
+    if (typeof marked === 'undefined') {
+        console.error("marked.js is not loaded");
+        return;
+    }
+
     fetch("https://api.github.com/repos/JeelDobariya38/Simple-Calculator/releases")
         .then(response => response.json())
         .then(releases => {
             console.log(releases);
+
+            // Find the "Other Releases" container
+            const otherReleasesContainer = document.querySelector('.other-version');
+            console.log(otherReleasesContainer)
+
             for (var release of releases) {
-                console.log(release.name);
-                console.log(release.body);
-                console.log(release.prerelease);
-                console.log(release.published_at);
-                var assets = release.assets;
-                for (var asset of assets) {
-                    if (asset.name.endsWith(".zip")) {
-                        console.log(asset.name);
-                        console.log(asset.browser_download_url);
-                        break;
-                    }
+                if (!release.draft) {
+                    const convertedBody = marked(release.body);
+                    const releaseElement = document.createElement('div');
+                    releaseElement.classList.add('release');
+                    releaseElement.innerHTML = `
+                        <h1>${release.name}</h1>
+                        <div class="info">
+                            <div><b>Prerelease: <span class="prerelease">${release.prerelease}</span></b></div>
+                            <div><b>Published At: <span class="published-at">${release.published_at}</span></b></div>
+                        </div>
+                        <p>${convertedBody}</p>
+                        <a href="${release.html_url}" target="_blank"><button>Download</button></a>
+                    `;
+                    otherReleasesContainer.appendChild(releaseElement);
                 }
             }
-        }).catch(error => console.error("Error fetching latest release:", error));
-    }
-);
+
+            // Check if there are no other releases and display an error message
+            if (otherReleasesContainer.children.length === 0) {
+                const errorMsg = document.querySelector('.other-version .error-msg');
+                errorMsg.style.display = 'block';
+            }
+        })
+        .catch(error => console.error("Error fetching latest release:", error));
+});
