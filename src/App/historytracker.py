b173cb9f6b25c70c7adfc9838ed5055e13f5772d
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-11-15 17:12:02 +0530

    Update historytracker.py

--- src/App/historytracker.py
+++ src/App/historytracker.py
@@ -2,13 +2,14 @@ class History:
     def __init__(self):
         self.history = []
 
-    def append_operation(self, expression: str):
-        self.history.append(expression)
+    def append_operation(self, expression: str, result: str):
+        self.history.append(expression + " -> " + result)
     
-    def print_history(self):
+    def __repr__(self):
+        string = ""
         for item in self.history:
-            print(item)
-        print()
+            string += item + "\n"
+        return string
 
     def clear_history(self):
         self.history = []
